52ca606b742e5925e372f0438553e432deeab1c9
Mladen Abaz <mladen.abaz@intellexi.hr>
2021-01-05 19:12:23 +0100

    project init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route} from "react-router-dom";
+import Home from "./pages/home/home";
+import Timesheet from "./pages/timesheet/timesheet";
+import Employee from "./pages/employee/employee";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <div className="App">
+                    <Route path="/" exact component={Home}/>
+                    <Route path="/timesheet" exact component={Timesheet}/>
+                    <Route path="/employee" exact component={Employee}/>
+                </div>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
