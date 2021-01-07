24ebcef96f3f00735eb99c991288af1cf10a11b7
Edi Štambuk <es50624@fer.hr>
2021-01-07 14:13:53 +0100

    Add text fields on login page

--- src/containers/Login/Login.tsx
+++ src/containers/Login/Login.tsx
@@ -1,15 +1,45 @@
+import * as React from 'react';
 import {Component} from 'react';
-import history from "../../history";
+import {Button, Container, TextField} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+
+import history from '../../history';
 import {ROUTE} from "../../routing/Routes";
+
+import './Login.css';
 
 export class Login extends Component {
 
+  public username = "";
+  public password = "";
+
     render() {
         return (
-            <div>
-                <h1>Login screen</h1>
-                <button onClick={this.goHome}>home screen</button>
-            </div>
+            <>
+              {/*<CssBaseline />*/}
+              <Container maxWidth="sm" className="Container">
+                <Typography component="div" className="Login">
+                  <div className="middle">
+                    <h1 className="title">Login</h1>
+                    <div>
+                      <div className="TextField Username">
+                        <TextField id="outlined-basic" label="Username" size="small"
+                                   variant="outlined" onChange={(e) => this.username = e.target.value}/>
+                      </div>
+                      <div className="TextField Password">
+                        <TextField id="outlined-basic" type="password" label="Password" size="small"
+                                   variant="outlined" onChange={(e) => this.password = e.target.value}/>
+                      </div>
+                    </div>
+                    <div className="goto">
+                      <Button onClick={this.goHome} variant="contained" color="primary">
+                        Log in
+                      </Button>
+                    </div>
+                  </div>
+                </Typography>
+              </Container>
+            </>
         );
     }
 
