52eed6dc13ec317d7c078bb7b53e5c2642cc508f
Edi Štambuk <es50624@fer.hr>
2021-01-11 13:25:03 +0100

    Authorize pages in router

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -7,8 +7,14 @@ import {ProtectedRoute} from "../../routing/ProtectedRoute";
 import Employee from "../Employee/Employee";
 import history from "../../history";
 import ClippedDrawer from "../../components/Drawer/Drawer";
+import {IAppState} from "app-store";
+import {connect} from "react-redux";
 
-export class Home extends Component {
+interface IProps {
+    isAuthentificated: boolean;
+}
+
+export class HomeInner extends Component<IProps> {
 
     public goToPage(path: string) {
         history.push(path);
@@ -21,13 +27,15 @@ export class Home extends Component {
     ]);
 
     render() {
+        const {isAuthentificated} = this.props;
+
         return (
             <>
                 <ClippedDrawer routes={this.routes} goToPage={this.goToPage}>
                     <Switch>
-                        <ProtectedRoute path={ROUTE.timesheet} exact component={Timesheet} hasAuthorizationRights={true}/>
-                        <ProtectedRoute path={ROUTE.dashboard} component={Dashboard} hasAuthorizationRights={true}/>
-                        <ProtectedRoute path={ROUTE.employee} component={Employee} hasAuthorizationRights={true}/>
+                        <ProtectedRoute path={ROUTE.timesheet} exact component={Timesheet} hasAuthorizationRights={isAuthentificated}/>
+                        <ProtectedRoute path={ROUTE.dashboard} component={Dashboard} hasAuthorizationRights={isAuthentificated}/>
+                        <ProtectedRoute path={ROUTE.employee} component={Employee} hasAuthorizationRights={isAuthentificated}/>
                         <Route>
                             <Redirect to={ROUTE.notFound}/>
                         </Route>
@@ -37,3 +45,11 @@ export class Home extends Component {
         )
     }
 }
+
+function mapStateToProps(state: IAppState) {
+    return {
+        isAuthentificated: state.auth.isAuthentificated
+    }
+}
+
+export const Home = connect(mapStateToProps, null)(HomeInner);
